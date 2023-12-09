90afd68c817aa70cc5081f4e29171ead19c45c15
vaneIv <appvane@gmail.com>
2023-12-09 18:57:41 +0100

    Created IntroductionViewModel and configured SharedPreferences to show the introduction fragment and login only once (the first time) when we open the app.

--- app/src/main/java/com/example/onlineshop/di/AppModule.kt
+++ app/src/main/java/com/example/onlineshop/di/AppModule.kt
@@ -1,5 +1,8 @@
 package com.example.onlineshop.di
 
+import android.app.Application
+import android.content.Context.MODE_PRIVATE
+import com.example.onlineshop.util.Constants.INTRODUCTION_SHARED_PREFERENCES
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -20,4 +23,9 @@ object AppModule {
     @Provides
     @Singleton
     fun provideFirebaseFirestore() = Firebase.firestore
+
+    @Provides
+    fun provideIntroductionSharedPreferences(
+        application: Application
+    ) = application.getSharedPreferences(INTRODUCTION_SHARED_PREFERENCES, MODE_PRIVATE)
 }
--- app/src/main/java/com/example/onlineshop/util/Constants.kt
+++ app/src/main/java/com/example/onlineshop/util/Constants.kt
@@ -3,4 +3,8 @@ package com.example.onlineshop.util
 object Constants {
 
     const val USER_COLLECTION = "user"
+
+    const val INTRODUCTION_SHARED_PREFERENCES = "IntroductionSharedPreferences"
+
+    const val INTRODUCTION_KEY = "IntroductionKey"
 }
