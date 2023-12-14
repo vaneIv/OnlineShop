6f78f91101755b5588042ffdeb8f3241a6e4874a
vaneIv <appvane@gmail.com>
2023-12-14 17:44:46 +0100

    Base category fragment setup

--- app/src/main/java/com/example/onlineshop/fragments/categories/BaseCategoryFragment.kt
+++ app/src/main/java/com/example/onlineshop/fragments/categories/BaseCategoryFragment.kt
@@ -1,7 +1,56 @@
 package com.example.onlineshop.fragments.categories
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.onlineshop.R
+import com.example.onlineshop.adapters.BestProductsAdapter
+import com.example.onlineshop.databinding.FragmentBaseCategoryBinding
+import com.example.onlineshop.databinding.FragmentMainCategoryBinding
 
 open class BaseCategoryFragment : Fragment(R.layout.fragment_base_category) {
+
+    private lateinit var binding: FragmentBaseCategoryBinding
+
+    private lateinit var offerAdapter: BestProductsAdapter
+    private lateinit var bestProductsAdapter: BestProductsAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBaseCategoryBinding.inflate(inflater)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupOfferRecyclerView()
+        setupBestProductsRecyclerView()
+    }
+
+    private fun setupBestProductsRecyclerView() {
+        bestProductsAdapter = BestProductsAdapter()
+        binding.recyclerViewBestProducts.apply {
+            layoutManager =
+                GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+            adapter = bestProductsAdapter
+        }
+    }
+
+    private fun setupOfferRecyclerView() {
+        offerAdapter = BestProductsAdapter()
+        binding.recyclerViewOfferProducts.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+            adapter = offerAdapter
+        }
+    }
 }
