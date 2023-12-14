e9eac550e05c8bb0694d11281a6e62aabb2dc21c
vaneIv <appvane@gmail.com>
2023-12-14 13:28:21 +0100

    Paging setup for BestProducts in MainCategoryViewModel.

--- app/src/main/java/com/example/onlineshop/fragments/categories/MainCategoryFragment.kt
+++ app/src/main/java/com/example/onlineshop/fragments/categories/MainCategoryFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
@@ -102,24 +103,30 @@ class MainCategoryFragment : Fragment(R.layout.fragment_main_category) {
             viewModel.bestProducts.collectLatest {
                 when (it) {
                     is Resource.Loading -> {
-                        showLoading()
+                        binding.progressBarBestProduct.visibility = View.VISIBLE
                     }
 
                     is Resource.Success -> {
                         bestProductsAdapter.differ.submitList(it.data)
-                        hideLoading()
+                        binding.progressBarBestProduct.visibility = View.GONE
                     }
 
                     is Resource.Error -> {
-                        hideLoading()
                         Log.e(TAG, it.message.toString())
                         Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                        binding.progressBarBestProduct.visibility = View.GONE
                     }
 
                     else -> Unit
                 }
             }
         }
+
+        binding.nestedScrollviewMainCategory.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { v, _, scrollY, _, _ ->
+            if (v.getChildAt(0).bottom <= v.height + scrollY) {
+                viewModel.fetchBestProducts()
+            }
+        })
     }
 
     private fun setupBestProductsRv() {
