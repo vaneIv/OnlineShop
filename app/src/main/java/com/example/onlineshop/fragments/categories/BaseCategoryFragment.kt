69e2a4e50e8cc5c76f5e53236261141bc31fefe9
vaneIv <appvane@gmail.com>
2023-12-16 16:33:17 +0100

    Added Category class, CategoryViewModel, also modify the category fragments.

--- app/src/main/java/com/example/onlineshop/fragments/categories/BaseCategoryFragment.kt
+++ app/src/main/java/com/example/onlineshop/fragments/categories/BaseCategoryFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.onlineshop.R
 import com.example.onlineshop.adapters.BestProductsAdapter
 import com.example.onlineshop.databinding.FragmentBaseCategoryBinding
@@ -16,8 +18,8 @@ open class BaseCategoryFragment : Fragment(R.layout.fragment_base_category) {
 
     private lateinit var binding: FragmentBaseCategoryBinding
 
-    private lateinit var offerAdapter: BestProductsAdapter
-    private lateinit var bestProductsAdapter: BestProductsAdapter
+    protected val offerAdapter: BestProductsAdapter by lazy { BestProductsAdapter() }
+    protected val bestProductsAdapter: BestProductsAdapter by lazy { BestProductsAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,10 +36,50 @@ open class BaseCategoryFragment : Fragment(R.layout.fragment_base_category) {
 
         setupOfferRecyclerView()
         setupBestProductsRecyclerView()
+
+        binding.recyclerViewOfferProducts.addOnScrollListener(object :
+            RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                if (!recyclerView.canScrollVertically(1) && dx != 0) {
+                    onOfferPagingRequest()
+                }
+            }
+        })
+
+        binding.nestedScrollviewBaseCategory.setOnScrollChangeListener(NestedScrollView.OnScrollChangeListener { v, _, scrollY, _, _ ->
+            if (v.getChildAt(0).bottom <= v.height + scrollY) {
+                onBestProductsPagingRequest()
+            }
+        })
+    }
+
+    fun showOfferLoading() {
+        binding.progressBarOfferProducts.visibility = View.VISIBLE
+    }
+
+    fun hideOfferLoading() {
+        binding.progressBarOfferProducts.visibility = View.GONE
+    }
+
+    fun showBestProductsLoading() {
+        binding.progressBarBestProducts.visibility = View.VISIBLE
+    }
+
+    fun hideBestProductsLoading() {
+        binding.progressBarBestProducts.visibility = View.GONE
+    }
+
+    open fun onOfferPagingRequest() {
+
+    }
+
+    open fun onBestProductsPagingRequest() {
+
     }
 
     private fun setupBestProductsRecyclerView() {
-        bestProductsAdapter = BestProductsAdapter()
         binding.recyclerViewBestProducts.apply {
             layoutManager =
                 GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
@@ -46,7 +88,6 @@ open class BaseCategoryFragment : Fragment(R.layout.fragment_base_category) {
     }
 
     private fun setupOfferRecyclerView() {
-        offerAdapter = BestProductsAdapter()
         binding.recyclerViewOfferProducts.apply {
             layoutManager =
                 LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
