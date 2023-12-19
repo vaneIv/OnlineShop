3f26e970e27b850211102189144881656f51b267
vaneIv <appvane@gmail.com>
2023-12-19 21:30:48 +0100

    Added CartProductsAdapter and CartViewModel.

--- app/src/main/java/com/example/onlineshop/activities/ShoppingActivity.kt
+++ app/src/main/java/com/example/onlineshop/activities/ShoppingActivity.kt
@@ -2,11 +2,18 @@ package com.example.onlineshop.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.onlineshop.R
 import com.example.onlineshop.databinding.ActivityShoppingBinding
+import com.example.onlineshop.util.Resource
+import com.example.onlineshop.viewmodel.CartViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class ShoppingActivity : AppCompatActivity() {
@@ -15,11 +22,31 @@ class ShoppingActivity : AppCompatActivity() {
         ActivityShoppingBinding.inflate(layoutInflater)
     }
 
+    val viewModel by viewModels<CartViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         val navController = findNavController(R.id.shopping_nav_host_fragment)
         binding.bottomNavigation.setupWithNavController(navController)
+
+        lifecycleScope.launch {
+            viewModel.cartProducts.collectLatest {
+                when (it) {
+                    is Resource.Success -> {
+                        val count = it.data?.size ?: 0
+                        val bottomNavigation =
+                            findViewById<BottomNavigationView>(R.id.bottom_navigation)
+                        bottomNavigation.getOrCreateBadge(R.id.cartFragment).apply {
+                            number = count
+                            backgroundColor = resources.getColor(R.color.g_blue)
+                        }
+                    }
+
+                    else -> Unit
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/onlineshop/adapters/CartProductsAdapter.kt
@@ -0,0 +1,85 @@
+package com.example.onlineshop.adapters
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.onlineshop.data.CartProduct
+import com.example.onlineshop.databinding.CartProductItemBinding
+import com.example.onlineshop.util.getProductPrice
+
+class CartProductsAdapter : RecyclerView.Adapter<CartProductsAdapter.CartProductsViewHolder>() {
+
+    inner class CartProductsViewHolder(val binding: CartProductItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(cartProduct: CartProduct) {
+            binding.apply {
+                Glide.with(itemView).load(cartProduct.product.images[0]).into(imageViewCartProduct)
+                textViewProductCartName.text = cartProduct.product.name
+                textViewCartProductQuantity.text = cartProduct.quantity.toString()
+
+                val priceAfterPercentage =
+                    cartProduct.product.offerPercentage.getProductPrice(cartProduct.product.price)
+                textViewProductCartPrice.text = "$ ${String.format("%.2f", priceAfterPercentage)}"
+
+                imageCartProductColor.setImageDrawable(
+                    ColorDrawable(
+                        cartProduct.selectedColor ?: Color.TRANSPARENT
+                    )
+                )
+                textViewCartProductSize.text = cartProduct.selectedSize ?: "".also {
+                    imageCartProductSize.setImageDrawable(ColorDrawable(Color.TRANSPARENT))
+                }
+            }
+        }
+    }
+
+    private val diffCallback = object : DiffUtil.ItemCallback<CartProduct>() {
+        override fun areItemsTheSame(oldItem: CartProduct, newItem: CartProduct): Boolean {
+            return oldItem.product.id == newItem.product.id
+        }
+
+        override fun areContentsTheSame(oldItem: CartProduct, newItem: CartProduct): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, diffCallback)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CartProductsViewHolder {
+        return CartProductsViewHolder(
+            CartProductItemBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(holder: CartProductsViewHolder, position: Int) {
+        val cartProduct = differ.currentList[position]
+        holder.bind(cartProduct)
+
+        holder.itemView.setOnClickListener {
+            onProductClick?.invoke(cartProduct)
+        }
+
+        holder.binding.imageViewMinus.setOnClickListener {
+            onMinusClick?.invoke(CartProduct())
+        }
+
+        holder.binding.imageViewPlus.setOnClickListener {
+            onPlusClick?.invoke(CartProduct())
+        }
+    }
+
+    var onProductClick: ((CartProduct) -> Unit)? = null
+    var onPlusClick: ((CartProduct) -> Unit)? = null
+    var onMinusClick: ((CartProduct) -> Unit)? = null
+}
